3f1da22cd45be5fba3b12a9a1cd8472c2b891c4d
YauzZ <yauzz.scu@gmail.com>
2009-04-09 14:14:35 +0800

    初始化，建立crawler类

---
+++ searchengine.py
@@ -0,0 +1,23 @@
+
+
+
+class crawler:
+	def __init__(self,dbname):
+		pass
+
+	def __del__(self):
+		pass
+	def dbcommit(self):
+		pass
+	
+	def geientryid(self,table,field,value,createnew=True):
+		return None
+	
+	def addtoindex(self,url,soup):
+		print 'Indexing %s' % url
+
+	def gettextonly(self,soup):
+		return None
+
+	def separatewords(self,text):
+		return None
