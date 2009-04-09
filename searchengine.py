fe89dbc4734dc3929f61cb007344553e09c7fee3
YauzZ <yauzz.scu@gmail.com>
2009-04-09 15:08:14 +0800

    添加爬虫程序及其测试代码

--- searchengine.py
+++ searchengine.py
@@ -1,5 +1,8 @@
+import urllib2
+from BeautifulSoup import *
+from urlparse import urljoin
 
-
+ignorewords=set(['the','of','to','and','a','in','is','it'])
 
 class crawler:
 	def __init__(self,dbname):
@@ -29,7 +32,33 @@ class crawler:
 		pass
 
 	def crawl(self,pages,depth=2):
-		pass
+		for i in range(depth):
+			newpages=set()
+			for page in pages:
+				try:
+					c=urllib2.urlopen(page)
+				except:
+					print "Could not open %s" % page
+					continue
+				soup=BeautifulSoup(c.read())
+				self.addtoindex(page,soup)
+
+				links=soup('a')
+				for link in links:
+					if('href' in dict(link.attrs)):
+						url=urljoin(page,link['href'])
+						if url.find("'")!=-1: continue
+						url=url.split('#')[0]
+						if url[0:4]=='http' and not self.isindexed(url):
+							newpages.add(url)
+						linkText=self.gettextonly(link)
+						self.addlinkref(page,url,linkText)
+
+				self.dbcommit()
+			pages=newpages
 
 	def createindextables(self):
 		pass
+
+if __name__ == "__main__":
+	print "searchengine ok"
